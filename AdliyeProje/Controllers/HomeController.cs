52183513a7a34f70c0c70d268a0cfbcb5bba8b12
adaletinsesi15 <adaletinsesi15@gmail.com>
2020-11-29 16:02:51 +0300

    Initial commit

---
+++ AdliyeProje/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AdliyeProje.Controllers;
+
+namespace AdliyeProje.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ AdliyeProje/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AdliyeProje.MvcApplication" Language="C#" %>
