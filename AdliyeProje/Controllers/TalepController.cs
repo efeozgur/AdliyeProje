64290228676bd7f2f5be1777b02d248fdae9fb73
efeozgur <efecuneytozgur@gmail.com>
2020-12-01 15:34:35 +0300

    Talep Ekle

---
+++ AdliyeProje/Controllers/TalepController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AdliyeProje.Controllers
+{
+    public class TalepController : Controller
+    {
+        // GET: Talep
+        public ActionResult TalepEkle()
+        {
+            return View();
+        }
+    }
+}
